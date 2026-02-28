a225f95bea9a83d4819772a2f033a0e0be58b8b0
AlanVultaggio <alan@unfoldingcreative.com>
2026-02-27 21:54:16 -0500

    Update checkout response to include Stripe status

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -1,7 +1,10 @@
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
 exports.handler = async () => {
   return {
     statusCode: 200,
     headers: { "content-type": "application/json" },
-    body: JSON.stringify({ ok: true }),
+    body: JSON.stringify({ stripeLoaded: !!stripe }),
   };
 };
